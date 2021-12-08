7592312d26ed8fe86b1abcb6c39cae1422e29e56
Justin Croteau <justinwebdev1@gmail.com>
2021-12-07 18:25:50 -0600

    opps

---
+++ script.js
@@ -0,0 +1,69 @@
+const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1ceb7d8c28f788644be431a87a61dea1&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?&api_key=1ceb7d8c28f788644be431a87a61dea1&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+// const form = document.getElementById('form')
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results);
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value 
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+
+});
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach(movie => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEL = document.createElement('div');
+        movieEL.classList.add('movie');
+        
+        movieEL.innerHTML = ` <div class="movie">
+                                <img src="${IMG_PATH + poster_path}" alt="${title}">
+                                <div class="movie-info">
+                                    <h3>${title}</h3>
+                                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                                </div>
+                                <div class="overview">
+                                    <h3>Overview</h3>
+                                    ${overview}
+                                </div>
+        
+                            `
+
+       main.appendChild(movieEL); 
+    })
+}
+
+function getClassByRate(vote) {
+    
+    if (vote >= 7.5) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'        
+    } else {
+        return 'red'       
+    }
+}
